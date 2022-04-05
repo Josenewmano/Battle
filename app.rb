89115ed6712d660f983962105524782db63e44ed
Joe <j.newman@live.co.uk>
2022-04-05 15:48:53 +0100

    Named cat POST

--- app.rb
+++ app.rb
@@ -9,9 +9,17 @@ get '/secret' do
     'Shhh!'
 end
 
-get '/cat' do
+get '/random-cat' do
    @name = ["Amigo", "Misty", "Almond"].sample
    erb(:index)
 end
 
-
+post '/named-cat' do
+    p params
+    @name = params[:name]
+    erb(:index)
+ end
+ 
+get '/form' do
+    erb(:index)
+end
